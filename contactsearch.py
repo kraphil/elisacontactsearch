353b950030a912b3fd84f096cbf68a8571ee28f8
Philippe Krajsic <philippekrajsic@gmail.com>
2021-12-01 15:39:07 +0100

    Update contactsearch.py

--- contactsearch.py
+++ contactsearch.py
@@ -26,9 +26,9 @@ def extractZip(userMessage):
     if(len(userMessage) == 0):
         zip = ""
     else:
-        #zip = userMessage['output'][0]['slotFillingParameter']['plz']
+        zip = userMessage['messages'][0]['subbot']['slotFillingParameter']['plz']
         #zip = userMessage['messages'][0]['data']['content']
-        print(userMessage)
+        #print(userMessage)
     return zip
 
 def extractConversationId(userMessage):
